9f3dc3ec184e58a98e3927d25e5e427241d9c038
nachogoca <nacho.gon.car@gmail.com>
2017-02-27 18:02:13 -0600

    feat: add database models

--- mysite/rain/models.py
+++ mysite/rain/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Observatory(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=30)
+    creation_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class ObservatoryLocation(models.Model):
+    observatory = models.ForeignKey(Observatory)
+    location = models.PointField(srid=4326)
+
+    def __str__(self):
+        return self.location
+
+class PrecipitationMeasurement(models.Model):
+    observatory_location = models.ForeignKey(ObservatoryLocation)
+    precipitation_24hr = models.FloatField()
+    rainfall_rate = models.FloatField()
+
+    def __str__(self):
+        return u'%s %s' % (self.precipitation_24hr, self.rainfall_rate)
+
