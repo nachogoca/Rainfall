f5590f070cf9dd4ea28ecd8295f1c61ae18a9df9
nachogoca <nacho.gon.car@gmail.com>
2017-03-09 21:46:11 -0600

    style: change static dirs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,5 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Redirect login
 LOGIN_REDIRECT_URL = '/'
