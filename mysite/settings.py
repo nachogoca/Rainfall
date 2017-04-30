c3cdb1b05154dff6a4427be805e7f47056dc3f7b
nachogoca <nacho.gon.car@gmail.com>
2017-04-29 22:36:49 -0500

    fix: allow hami1981.pythonwhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w0=^(h@c#wysygfc(b%s+3b&lpbcgjdruz8-c_ffwh8^w3564p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hami1981.pythonanywhere.com']
 
 
 # Application definition
