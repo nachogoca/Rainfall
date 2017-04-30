18e67289c687f145c38d7dc461a6d6736a890911
nachogoca <nacho.gon.car@gmail.com>
2017-04-29 22:39:47 -0500

    fix: bug in download form

--- rain/forms.py
+++ rain/forms.py
@@ -30,8 +30,7 @@ class DateInput(forms.DateTimeInput):
 
 
 class DownloadForm(forms.ModelForm):
-    #choices = [[x.id, x] for x in Observatory.objects.all()]
-    choices = []
+    choices = [[x.id, x] for x in Observatory.objects.all()]
     observatory_choices = forms.MultipleChoiceField(choices=choices)
     start_date = forms.DateTimeField(widget=DateInput())
     end_date = forms.DateTimeField(widget=DateInput())
