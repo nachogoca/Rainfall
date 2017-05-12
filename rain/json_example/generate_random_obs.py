0899706dfcd75d510a60ef89c73ce83d7ace823f
nachogoca <nacho.gon.car@gmail.com>
2017-05-11 22:30:37 -0500

    fix: generate random observations script

--- rain/json_example/generate_random_obs.py
+++ rain/json_example/generate_random_obs.py
@@ -7,22 +7,11 @@ START_DATE = datetime.datetime(day=1, month=1, year=2014)
 END_DATE = datetime.datetime(day=2, month=1, year=2014)
 
 
-class Precipitation():
-    measure_datetime = None
-    rainfall_rate = None
-
-    def __init__(self, measure_datetime, rainfall_rate):
-        self.measure_datetime = measure_datetime
-        self.rainfall_rate = rainfall_rate
-
-
 def main():
     observations = generate_random_observations()
 
     with open('data.json', 'w') as openfile:
-        for obs in observations:
-            json.dump(obs.__dict__, openfile, default=json_serial)
-
+        json.dump(observations, openfile, indent=4,sort_keys=True)
 
 
 def generate_random_observations():
@@ -32,19 +21,14 @@ def generate_random_observations():
     while d < END_DATE:
 
         rand = uniform(0, 15)
-        precipitation = Precipitation(d, rand)
-        observations.append(precipitation)
+        precip = {}
+        precip['measure_datetime'] = d.isoformat()
+        precip['rainfall_rate'] = rand
+        observations.append(precip)
 
         d += INTERVAL
 
     return observations
 
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, datetime.datetime):
-        serial = obj.isoformat()
-        return serial
-    raise TypeError ("Type not serializable")
 
 main()
